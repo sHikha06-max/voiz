a70f6153919e782965d229e02e177d5c08e34acb
Shikha Mishra <Shikha@Amits-MacBook-Air.local>
2022-09-10 21:05:15 +0530

    added drop down and search option and multioptions

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React, { useState } from "react";
 import "./App.css";
-
+import Select from "react-select";
+import option from "./Options";
 
 export default function App() {
-  
+ 
+  const [selectedOptions, setSelectedOptions] = useState();
 
-  
+  function handleSelect(data) {
+    setSelectedOptions(data);
+  }
   return (
     <div className="app">
-<h2>Select box</h2>
-      
+      <h2>Select box</h2>
+      <div className="dropdown-box">
+        <Select
+          options={option}
+          placeholder="Choose options"
+          value={selectedOptions}
+          onChange={handleSelect}
+          isMulti
+          isSearchable={true}
+          
+        />
+      </div>
     </div>
   );
 }
---
+++ src/Options.jsx
@@ -0,0 +1,13 @@
+const option = [
+    { value: "red", label: "Red" },
+    { value: "green", label: "Green" },
+    { value: "purple", label: "Purple" },
+    { value: "blue", label: "Blue" },
+    { value: "white", label: "White" },
+    { value: "brown", label:"Brown"},
+    { value: "pink", label:" Pink"},
+    { value: "crimson", label:"Crimson"},
+    { value: "black", label: "Black"},
+  ];
+export default option;
+
